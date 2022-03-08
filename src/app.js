9109fdeea6314887d804296329176384adb6bd59
kailinwei <kailinwei95@gmail.com>
2022-03-08 11:26:29 -0800

    merge

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
-import Programs from './pages/programs';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import './styles/App.css';
+import Programs from "./pages/programs";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import "./styles/App.css";
 
 const App = () => {
-    return (
-        <>
-            <Routes>
-                
-                <Route path="/programs" element={<Programs />}></Route>
-                <Route path="/home" element={<Home />}></Route>
-            </Routes>
-        </>
-
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/programs" element={<Programs />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
-
-
-
