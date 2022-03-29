cab2eb81a1641e7e96f5c4e4809aa6ae35cea5bd
Mai Toyoda <maiT-horse@hotmail.com>
2022-03-29 04:35:52 -0700

    styling for write review page and program detail page

--- src/pages/writeReviews.js
+++ src/pages/writeReviews.js
@@ -3,15 +3,22 @@ import { useState, useEffect } from 'react';
 import "../styles/writeReviews.css";
 import baseUrl from "../data/baseApi";
 import { useParams } from 'react-router-dom';
-
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { useNavigate } from 'react-router-dom';
 const writeReviews = (props) => {
     const {schoolProgramId} = useParams();
     const [score, setScore] = useState(0);
     const [review, setReview] = useState("");
     const [message, setMessage] = useState("");
 
+    const navigate = useNavigate();
     let handleSubmit = async (e) => {
         e.preventDefault();
+
         // console.log({SchoolProgramId: schoolProgramId,
         //     RateNumber: Number(score),
         //     ProgramReview: review,})
@@ -37,31 +44,41 @@ const writeReviews = (props) => {
             } else {
                 setMessage("Some error occured");
             }
+            navigate("/thankyou");
         } catch (err) {
             console.log(err);
         }
+
+
     };
 
 
     return (
+        <Container className="p-3">
+        
         <div className="write-reviews-wrapper">
             <h2>Write Reviews</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Score</label>
-                <input
-                    type="text"
-                    value={score}
-                    onChange={(e) => setScore(e.target.value)}
-                /><br/>
-                <label>Reasons</label>
-                <textarea
-                    type="text"
-                    value={review}
-                    onChange={(e) => setReview(e.target.value)}
-                />  
-                <button type="submit">Submit</button>
-            </form>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalScore">
+                        <Form.Label column sm={2}>
+                            Score
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control value={score} min='0' max='5' type="text" onChange={(e) => setScore(e.target.value)} placeholder="Score" />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalReview">
+                        <Form.Label column sm={2}>
+                            Reasons
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control value={review} type="text" as="textarea" onChange={(e) => setReview(e.target.value)} placeholder="Review" />
+                        </Col>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
         </div>
+        </Container>
     )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
     {
     test: /\.css$/,
     use: ["style-loader", "css-loader"]
-    }
+    },
+    {
+        test: /\.(gif|svg|jpg|png)$/,
+      loader: "file-loader",
+}
     ]},
     plugins: [htmlPlugin],
     devServer: {
